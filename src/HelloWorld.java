27082f6e58bb07bf733c484c9590f8f98f651ae3
Kapil Chouhan <kapilcho@infinite.com>
2025-10-13 23:26:53 +0530

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("HellOWorld");
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2/2);
+        System.out.println(2*2);
+        System.out.println(2%2);
+    }
+
+}
